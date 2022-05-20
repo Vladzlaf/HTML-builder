cb94f416b1d2aa4f1225caf5612e664f873beacb
Vladzlaf <vladzlafm@gmail.com>
2022-05-20 23:26:53 +0300

    feat: add task 04 (Fri, May 20, 2022 11:26:24 PM)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,38 @@
+const path = require('path');
+const fs = require('fs');
+const { readdir } = require('fs')
+const fsPromises = fs.promises;
+
+const filesPath = path.join(__dirname ,'/files');
+const filesCopyPath = path.join(__dirname, '/file-copy');
+
+readdir(filesCopyPath, (err, files) => {
+    if (err) console.log('Error!');
+    else {
+        for (const file of files) {
+            fs.unlink(path.join(__dirname, '/file-copy', file), err => {
+                if (err) console.log('err1');
+            });
+        };
+    };
+});
+
+fsPromises.mkdir(filesCopyPath, {recursive: true}).then(function(){});
+
+readdir(filesPath, (err, files) => {
+    if (err) console.log('Error!');
+    else {
+        for (const file of files) {
+            const filePath = path.join(__dirname ,'/files', file);
+            const fileCopyPath = path.join(__dirname, '/file-copy', file);
+            fs.copyFile(filePath, fileCopyPath, err =>{
+                if (err) console.log('Error!');
+                else {
+                    console.log('Done!');
+                };
+            });        
+
+        };
+    };
+});
 
